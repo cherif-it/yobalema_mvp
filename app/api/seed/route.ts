515eebfd15c5e96f9355d430f714fd290207926d
DIOP TRAORE AMADOU CHERIF JUNIOR <traoreamadoucherifjuniordiop@cherif-macbook-pro.local>
2026-06-05 19:27:01 +0200

    ajout de app/api/seed/route.ts pour user

---
+++ app/api/seed/route.ts
@@ -0,0 +1,14 @@
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Test User",
+      email: "test@yobalema.com",
+      password: "123456",
+      role: "EXPEDITOR",
+    },
+  });
+
+  return Response.json(user);
+}
