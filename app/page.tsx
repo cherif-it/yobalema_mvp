afb0c5dcc387975dcc66c0a81324f5b8a4a877de
DIOP TRAORE AMADOU CHERIF JUNIOR <traoreamadoucherifjuniordiop@cherif-macbook-pro.local>
2026-06-07 18:48:47 +0200

    add frontend page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main style={{ padding: 40 }}>
-      <h1>Yobalema 🚚</h1>
-      <p>Transport de colis entre particuliers</p>
-    </main>
+    <div style={{ padding: 40 }}>
+      <h1>🚚 Yobalema MVP</h1>
+
+      <div style={{ display: "flex", gap: 20, marginTop: 20 }}>
+        <Link href="/create-shipment">📦 Créer un colis</Link>
+        <Link href="/create-availability">🚗 Créer un trajet</Link>
+        <Link href="/bookings">🤝 Bookings</Link>
+      </div>
+    </div>
   );
 }
