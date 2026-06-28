681e2ca83cb0176bfa3ae01fd77c2cefcc377c63
DIOP TRAORE AMADOU CHERIF JUNIOR <traoreamadoucherifjuniordiop@cherif-macbook-pro.local>
2026-06-28 18:02:15 +0200

    add callbacks

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -39,10 +39,19 @@ export const authOptions = {
   },
 
   callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+  
+      return token;
+    },
+  
     async session({ session, token }) {
       if (session.user) {
-        session.user.id = token.sub;
+        session.user.id = token.id as string;
       }
+  
       return session;
     },
   },
