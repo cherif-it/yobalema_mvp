affb4a36b95e9a58ed889864dfc3c4e7309dd2d9
DIOP TRAORE AMADOU CHERIF JUNIOR <traoreamadoucherifjuniordiop@cherif-macbook-pro1.home>
2026-07-28 20:40:10 +0200

    add utiles/errors.ts

---
+++ utils/errors.ts
@@ -0,0 +1,26 @@
+export class AppError extends Error {
+    status: number;
+  
+    constructor(message: string, status = 400) {
+      super(message);
+      this.status = status;
+    }
+  }
+  
+  export class UnauthorizedError extends AppError {
+    constructor(message = "Unauthorized") {
+      super(message, 401);
+    }
+  }
+  
+  export class ForbiddenError extends AppError {
+    constructor(message = "Forbidden") {
+      super(message, 403);
+    }
+  }
+  
+  export class NotFoundError extends AppError {
+    constructor(message = "Not Found") {
+      super(message, 404);
+    }
+  }
