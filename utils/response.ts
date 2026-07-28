4b6d172f3a950cb8172fd6544eb71eea069b64a7
DIOP TRAORE AMADOU CHERIF JUNIOR <traoreamadoucherifjuniordiop@cherif-macbook-pro1.home>
2026-07-28 20:39:08 +0200

    add folder

---
+++ utils/response.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export function success(data: unknown, status = 200) {
+  return NextResponse.json(data, { status });
+}
+
+export function failure(message: string, status = 400) {
+  return NextResponse.json(
+    {
+      success: false,
+      message,
+    },
+    { status }
+  );
+}
